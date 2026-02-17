ab02042a17e89f12617e521b85877c2ee4a58040
ÖMER ALTAMMO <74145656+Omaraltammo@users.noreply.github.com>
2026-02-17 15:01:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -127,3 +127,15 @@ loader.classList.add("hidden");
 }, 800);
 
 });
+/* MOUSE GLOW */
+
+const glow = document.querySelector(".mouse-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left=e.clientX+"px";
+
+glow.style.top=e.clientY+"px";
+
+});
+
